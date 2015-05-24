dbbc164596d63152b41108d5df32e42c5a7a1d85
Piotr Dabkowski <piodrus@gmail.com>
2015-05-24 18:55:22 +0100

    Good luck!

---
+++ pyjsparser/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['PyJsParser', 'Node', 'WrappingNode', 'node_to_dict']
+__author__ = 'Piotr Dabkowski'
+__version__ = '2.2.0'
+from pyjsparser import PyJsParser, Node, WrappingNode, node_to_dict
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyjsparser',
+    version='2.2.0',
+    packages=['pyjsparser'],
+    url='https://github.com/PiotrDabkowski/pyjsparser',
+    license='MIT',
+    author='Piotr Dabkowski',
+    author_email='piotr.dabkowski@balliol.ox.ac.uk',
+    description='Fast javascript parser (translation of esprima.js)'
+)
