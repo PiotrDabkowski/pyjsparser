190e6390bdaa4ffd5ea35fc9a61cfc298d4c008d
Ivan Ng <qwIvan@users.noreply.github.com>
2018-01-12 05:10:09 +0800

    update pyjsparser.__version__ to 2.5.2

--- pyjsparser/__init__.py
+++ pyjsparser/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ['PyJsParser', 'parse', 'JsSyntaxError']
 __author__ = 'Piotr Dabkowski'
-__version__ = '2.2.0'
+__version__ = '2.5.2'
 from .parser import PyJsParser, parse, JsSyntaxError
